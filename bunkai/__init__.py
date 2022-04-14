a468cfff1ab56af9bcc7591d0358f18065f78ae4
Yuta Hayashibe <yuta@hayashibe.jp>
2022-04-14 10:39:07 +0900

    v1.5.1

--- bunkai/__init__.py
+++ bunkai/__init__.py
@@ -3,5 +3,5 @@
 from bunkai.algorithm.bunkai_sbd.bunkai_sbd import BunkaiSentenceBoundaryDisambiguation as Bunkai
 
 __all__ = ["Bunkai"]
-__version_info__ = (1, 5, 0)
+__version_info__ = (1, 5, 1)
 __version__ = ".".join(map(str, __version_info__))
