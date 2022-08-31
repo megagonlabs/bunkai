634f66bc44d9cf6dd4c324c10446a1975845268b
Yuta Hayashibe <yuta@hayashibe.jp>
2022-08-31 16:20:24 +0900

    Use GitHub Actions

--- mks/lint.mk
+++ mks/lint.mk
@@ -1,22 +1,5 @@
 
 GREP_EXCLUDE:=grep -v -e '\.eggs' -e '\.git' -e 'pyc$$' -e '\.idea' -e 'venv' -e 'python_env' -e 'egg-info' -e htmlcov -e 'work_scripts' -e mks -e node_modules
-TERMS_CHECK_CMD:=grep -e split -e divide
-TERMS_CHECK_CONTENT_OPTION:=-e 文分割 -e 'coding: utf'
-
-POETRY_NO_ROOT:= --no-root
-POETRY_LB:= -E lb
-POETRY_TRAIN:= -E train
-
-dev_setup:
-	poetry install $(POETRY_NO_ROOT) $(POETRY_LB) $(POETRY_TRAIN) $(POETRY_OPTION)
-
-setup: setup_python setup_npm
-
-setup_python:
-	poetry install $(POETRY_LB) $(POETRY_TRAIN) $(POETRY_OPTION)
-
-setup_npm:
-	npm install
 
 
 TARGET_DIRS:=./bunkai ./tests ./example \( -type d -name '.venv' -prune \) -or -type f
@@ -40,27 +23,12 @@ jsonlint:
 yamllint:
 	find . -name '*.yml' -type f | grep -v node_modules | xargs yamllint --no-warnings
 
-terms_check_path:
-	# check some words are not included in file name
-	find . | $(GREP_EXCLUDE) | $(TERMS_CHECK_CMD); if [ $$? -eq 1 ]; then echo "pass term check"; else exit 1; fi
-
-term_check_method:
-	# check if some terms are wrote in python method names
-	find . -type f | $(GREP_EXCLUDE) | grep -e 'py$$' | xargs -L 1 python3 .circleci/show_method_names.py
-
-term_check_file_content:
-	# check if 文分割 is written somewhere in a file.
-	find . -type f | $(GREP_EXCLUDE) | grep -v -e 'Makefile' -e 'show_method_names.py' -e 'example.py' | xargs grep $(TERMS_CHECK_CONTENT_OPTION); last_var=$$? ; if [ $${last_var} -eq 1 ] || [ $${last_var} -eq 123 ]; then echo "pass file content"; else exit 1; fi
-
-check_firstline:
-	find . -type f | $(GREP_EXCLUDE) | grep -e 'py$$' | grep -v '__init__' | grep -v third | xargs python3 .circleci/check_head.py
-
 check_version:
-	python3 .circleci/check_version.py --py bunkai/__init__.py --toml pyproject.toml
+	python3 check_version.py --py bunkai/__init__.py --toml pyproject.toml
 
 
 
-lint: flake8 black pyright isort yamllint terms_check_path term_check_method term_check_file_content check_firstline pydocstyle check_version
+lint: flake8 black pyright isort yamllint pydocstyle check_version
 
 _run_isort:
 	isort -rc .
